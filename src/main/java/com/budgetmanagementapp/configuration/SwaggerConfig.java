ea683e3e42bd611c13e2937b57eaec11d0a5a713
israfilzadehemin <israfilzadehemin@gmail.com>
2021-12-06 15:59:46 +0400

    SWAGGER: Implement JWT for swagger requests

--- src/main/java/com/budgetmanagementapp/configuration/SwaggerConfig.java
+++ src/main/java/com/budgetmanagementapp/configuration/SwaggerConfig.java
@@ -1,9 +1,15 @@
 package com.budgetmanagementapp.configuration;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -13,9 +19,26 @@ import static springfox.documentation.builders.RequestHandlerSelectors.basePacka
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "token", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("JWT", authorizationScopes));
+    }
+
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
                 .select()
                 .apis(basePackage("com.budgetmanagementapp.controller"))
                 .paths(PathSelectors.any())
