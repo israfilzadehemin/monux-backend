0e8b10daaf394949823f9332f900a705bc1d49da
shafa <shafam@wandafi.com>
2022-02-14 16:48:31 +0400

    UPDATE-2 Add liquibase files for maximum size constraints

---
+++ src/main/resources/liquibase/1/changelog-1.8-size-constraint.sql
@@ -0,0 +1,17 @@
+alter table feedback
+    alter column description type varchar(5000);
+
+alter table transaction
+    alter column description type varchar(5000);
+
+alter table translation
+    alter column az type varchar(5000);
+
+alter table translation
+    alter column en type varchar(5000);
+
+alter table translation
+    alter column ru type varchar(5000);
+
+
+
